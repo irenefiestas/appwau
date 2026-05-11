d89b7fe443a79b6875e3fa1b7f153d1297ba4591
Irene Fiestas Fimia <irenefiestasf@gmail.com>
2026-05-11 16:14:25 +0200

    Enable Vite assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>AppWau</title>
 
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
